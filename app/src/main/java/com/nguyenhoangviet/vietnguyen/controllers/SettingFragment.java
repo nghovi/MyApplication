bf9ccbee35aee27f66b698eb1db7444b2823b189
Nguyen Hoang Viet <vietnh90@gmail.com>
2016-01-08 18:01:54 +0700

    try graph

--- app/src/main/java/com/nguyenhoangviet/vietnguyen/controllers/SettingFragment.java
+++ app/src/main/java/com/nguyenhoangviet/vietnguyen/controllers/SettingFragment.java
@@ -1,6 +1,7 @@
 package com.nguyenhoangviet.vietnguyen.controllers;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,10 +11,24 @@ import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.Switch;
 
+import com.activeandroid.query.Select;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 import com.nguyenhoangviet.vietnguyen.controllers.Book.BookDetailFragment;
+import com.nguyenhoangviet.vietnguyen.controllers.Task.TaskListFragment;
 import com.nguyenhoangviet.vietnguyen.core.controller.MyFragment;
 import com.nguyenhoangviet.vietnguyen.core.database.DBHelper;
+import com.nguyenhoangviet.vietnguyen.models.MyModel;
+import com.nguyenhoangviet.vietnguyen.models.Task;
 import com.nguyenhoangviet.vietnguyen.myapplication.R;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class SettingFragment extends MyFragment{
 
@@ -34,6 +49,7 @@ public class SettingFragment extends MyFragment{
 	public void buildLayout(){
 		buildPushNotification();
 		buildSeekBar();
+		buildGraph();
 		buildContact();
 	}
 
@@ -73,6 +89,101 @@ public class SettingFragment extends MyFragment{
 
 			}
 		});
+	}
+
+	public void buildGraph(){
+		GraphView graph = (GraphView)getView().findViewById(R.id.graph);
+		LineGraphSeries<DataPoint> allTasksSeries = makeAllTasksSeries();
+		LineGraphSeries<DataPoint> finishedTasksSeries = makeFinishedTasksSeries();
+		// new LineGraphSeries<DataPoint>(new DataPoint[]{new DataPoint(0, 1), new DataPoint(1, 5), new DataPoint(2, 3), new DataPoint(3, 2), new
+		// DataPoint(4, 6)});
+		// LineGraphSeries<DataPoint> series2 = new LineGraphSeries<DataPoint>(new DataPoint[]{new DataPoint(0, 2), new DataPoint(1, 3), new
+		// DataPoint(2, 19), new DataPoint(3, 3), new DataPoint(4, 6)});
+		// graph.setScaleX(0.2f);
+		// graph.setScaleY(1);
+		graph.setTitle(getString(R.string.fragment_setting_graph_title));
+		// graph.addSeries(allTasksSeries);
+		graph.addSeries(finishedTasksSeries);
+	}
+
+	private LineGraphSeries<DataPoint> makeAllTasksSeries(){
+		LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>();
+		return series;
+	}
+
+	private LineGraphSeries<DataPoint> makeFinishedTasksSeries(){
+		int dayNumm = 30;
+		List<Task> tasks = getTasksRecently(dayNumm);
+		Map<Date, ArrayList<Task>> map = getDateTasksMap(tasks);
+		List<DataPoint> dataPoints = new ArrayList<DataPoint>();
+		Calendar c = Calendar.getInstance();
+		int i = 0;
+		List<Date> keys = getKeys(dayNumm);
+		for(Date key : keys){
+			ArrayList<Task> dayTasks = map.get(key);
+			DataPoint dataPoint;
+			if(dayTasks != null){
+				dataPoint = new DataPoint(i++, dayTasks.size());
+			}else{
+				dataPoint = new DataPoint(i++, 0);
+			}
+			dataPoints.add(dataPoint);
+		}
+		DataPoint dataPointsarray[] = dataPoints.toArray(new DataPoint[dataPoints.size()]);
+		LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(dataPointsarray);
+		return series;
+	}
+
+	private Map<Date, ArrayList<Task>> getDateTasksMap(List<Task> tasks){
+		Map<Date, ArrayList<Task>> map = new HashMap<Date, ArrayList<Task>>();
+		for(Task task : tasks){
+			Date mapKey = buildKey(task.date);
+			if(map.containsKey(mapKey)){
+				map.get(mapKey).add(task);
+			}else{
+				ArrayList<Task> tasksOnDate = new ArrayList<Task>();
+				tasksOnDate.add(task);
+				map.put(mapKey, tasksOnDate);
+			}
+		}
+
+		return map;
+	}
+
+	private List<Date> getKeys(int day){
+		List<Date> dates = new ArrayList<>();
+		Calendar c = Calendar.getInstance();
+		for(int i = 0; i < day; i++){
+			c.add(Calendar.DATE, 0 - i);
+			Date key = buildKey(c.getTime());
+			dates.add(key);
+		}
+		return dates;
+	}
+
+	private Date buildKey(Date date){
+		Calendar origin = Calendar.getInstance();
+		origin.setTime(date);
+		Calendar c = Calendar.getInstance();
+		c.clear();
+		c.set(Calendar.YEAR, origin.get(Calendar.YEAR));
+		c.set(Calendar.DAY_OF_MONTH, origin.get(Calendar.DAY_OF_MONTH));
+		c.set(Calendar.MONTH, origin.get(Calendar.MONTH));
+		return c.getTime();
+	}
+
+	// todo select in query, not all
+	private List<Task> getTasksRecently(int day){
+		List<Task> result = new ArrayList<>();
+		Calendar c = Calendar.getInstance();
+		c.add(Calendar.DATE, 0 - day);
+		List<Task> allTasks = new Select().from(Task.class).execute();
+		for(Task task : allTasks){
+			if(task.date.compareTo(c.getTime()) > 0){
+				result.add(task);
+			}
+		}
+		return result;
 	}
 
 	public void buildContact(){
